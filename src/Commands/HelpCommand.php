ad22d5ac30e9c779252c5518036e11ae1e443c79
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2022-12-21 16:10:37 +0200

    Update HelpCommand.php

--- src/Commands/HelpCommand.php
+++ src/Commands/HelpCommand.php
@@ -23,6 +23,7 @@ class HelpCommand extends Command
   php artisan billing:install stable {license key}(optional) - install stable version
   php artisan billing:install dev {license key}(optional) - install dev version(no recommend!!!)
   php artisan billing:uninstall - Removes the module and reinstalls the Pterodactyl panel
+  php artisan billing:fix - Fixes bugs with the old installer
   ';
     return $this->info($help);
   }
