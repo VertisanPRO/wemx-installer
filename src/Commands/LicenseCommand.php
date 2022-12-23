388c074c29358ff09e7864fb3cb3c39122fb2349
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2022-12-23 19:01:54 +0200

    Create LicenseCommand.php

---
+++ src/Commands/LicenseCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Billing\Commands\Commands;
+
+use Illuminate\Console\Command;
+
+class LicenseCommand extends Command
+{
+
+  protected $signature = 'billing:license {lic_key}';
+  protected $description = 'Save license key to DB Billing Module for Pterodactyl';
+
+  public function handle()
+  {
+    $this->setLicense();
+  }
+
+  private function setLicense()
+  {
+    $lic = Bill::settings();
+    $lic->name = 'license_key';
+    $lic->data = $this->argument('lic_key');
+    $lic->save();
+    return $this->info('License save to DB ' . $this->argument('lic_key'));
+  }
+}
