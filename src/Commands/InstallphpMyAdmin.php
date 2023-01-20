165a8431e30c4b9f52f9e35ba876627b597b4e54
PadowYT2 <me@padow.ru>
2023-01-20 13:19:14 +0300

    Fix everything in phpMyAdmin

--- src/Commands/InstallphpMyAdmin.php
+++ src/Commands/InstallphpMyAdmin.php
@@ -34,29 +34,27 @@ class InstallphpMyAdmin extends Command
             'What Webserver are you running?',
             self::WEBSERVER
         );
-        switch ($this->variables['OS_VERSIONS']) {
-            case 'debian':
-                $path_nginx = '/etc/nginx/sites-available/pterodactyl.conf';
-                $path_apache = '/etc/apache/sites-available/pterodactyl.conf';
-                $restart_nginx = 'systemctl restart nginx';
-                $restart_apache = 'systemctl restart apache2';
-                break;
-            case 'centos':
-                $path_nginx = '/etc/nginx/conf.d/pterodactyl.conf';
-                $path_apache = '/etc/httpd/conf.d/pterodactyl.conf';
-                $restart_nginx = 'systemctl restart nginx';
-                $restart_apache = 'systemctl restart httpd';
-                break;
-        }
         chdir('..');
         if (!file_exists('phpmyadmin')) {
+            switch ($this->variables['OS_VERSIONS']) {
+                case 'debian':
+                    $path_nginx = '/etc/nginx/sites-available/pterodactyl.conf';
+                    $path_apache = '/etc/apache2/sites-available/pterodactyl.conf';
+                    $restart_nginx = 'systemctl restart nginx';
+                    $restart_apache = 'systemctl restart apache2';
+                    break;
+                case 'centos':
+                    $path_nginx = '/etc/nginx/conf.d/pterodactyl.conf';
+                    $path_apache = '/etc/httpd/conf.d/pterodactyl.conf';
+                    $restart_nginx = 'systemctl restart nginx';
+                    $restart_apache = 'systemctl restart httpd';
+                    break;
+            }
             mkdir('phpmyadmin');
             chdir('phpmyadmin');
             $data = file_get_contents('https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip');
             @file_put_contents('phpMyAdmin.zip', $data);
-            exec('unzip -q -o phpMyAdmin.zip -d .');
-            unlink('phpMyAdmin.zip');
-            exec('mv phpMyAdmin-*/* .');
+            exec('unzip -q -o phpMyAdmin.zip -d . && rm phpMyAdmin.zip && mv phpMyAdmin-*/* .');
             $this->rmrfdir('phpMyAdmin-*');
             $this->info('Files installed, configuring webserver');
             if ($this->variables['OS_VERSIONS'] == 'nginx' && file_exists($path_nginx) == true) {
@@ -73,7 +71,7 @@ class InstallphpMyAdmin extends Command
                 file_put_contents($path_nginx, $file_contents);
                 exec($restart_nginx);
                 $this->info('phpMyAdmin has been successfully installed. It is available on ' . env('APP_URL') . '/phpmyadmin');
-            } elseif ($this->variables['OS_VERSIONS'] == 'apache' && file_exists($path_nginx) == true) {
+            } elseif ($this->variables['OS_VERSIONS'] == 'apache' && file_exists($path_apache) == true) {
                 $file_contents = file_get_contents($path_apache);
                 $lines = explode("\n", $file_contents);
                 array_pop($lines);
@@ -87,7 +85,7 @@ class InstallphpMyAdmin extends Command
                 file_put_contents($path_apache, $file_contents);
                 exec($restart_apache);
                 $this->info('phpMyAdmin has been successfully installed. It is available on ' . env('APP_URL') . '/phpmyadmin');
-            } elseif ($this->variables['OS_VERSIONS'] == 'nginx' && file_exists($path_nginx) == false || $this->variables['OS_VERSIONS'] == 'apache' && file_exists($path_nginx) == false) {
+            } elseif ($this->variables['OS_VERSIONS'] == 'nginx' && file_exists($path_nginx) == false || $this->variables['OS_VERSIONS'] == 'apache' && file_exists($path_apache) == false) {
                 $this->warn('You installed the Webserver config into a different file. You have to configure it yourself');
                 $this->warn('Otherwise you do not have the selected Webserver installed');
                 $this->info('phpMyAdmin has been partially installed');
@@ -102,14 +100,15 @@ class InstallphpMyAdmin extends Command
                 return;
             }
             $this->rmrfdir('phpmyadmin');
-            self::handle;
+            $this->handle();
         }
         if (!$this->confirm('Would you like to create a MySQL account that will be available for phpMyAdmin?')) {
             $this->warn('User was not created');
 
             return;
         }
-        return CreateMySQLUser::handle;
+        $create = new CreateMySQLUser();
+        return $create->handle();
     }
 
     public function rmrfdir($dir)
