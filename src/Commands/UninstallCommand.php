3cf3338b85232ff405182845a18664567ba8e9fb
PadowYT2 <me@padow.ru>
2023-01-20 13:29:18 +0300

    Remove comments & add installation of the installer

--- src/Commands/UninstallCommand.php
+++ src/Commands/UninstallCommand.php
@@ -12,12 +12,15 @@ class UninstallCommand extends Command
 
     public function handle()
     {
+        $installer = 0;
         if (!$this->confirm('Are you sure you want to continue with the uninstallation?')) {
             $this->warn('Billing Module was not uninstalled');
 
             return;
         }
-        // Remove all files command
+        if ($this->confirm('Would you like to remove the installer also?')) {
+            $installer = 1;
+        }
         $commands = [
             'rm -rf app/Models/Billing >/dev/null 2>&1 &',
             'rm -rf app/Models/Plugins >/dev/null 2>&1 &',
@@ -26,12 +29,6 @@ class UninstallCommand extends Command
             'rm -rf app/Http/Controllers/Plugins >/dev/null 2>&1 &',
             'rm -rf app/Http/Middleware/GMD.php >/dev/null 2>&1 &',
             'rm -rf database/migrations/billing >/dev/null 2>&1 &',
-            // 'rm -rf public/billing-src >/dev/null 2>&1 &',
-            // 'rm -rf public/modules/register >/dev/null 2>&1 &',
-            // 'rm -rf public/modules/plugins >/dev/null 2>&1 &',
-            // 'rm -rf public/themes/corbon >/dev/null 2>&1 &',
-            // 'rm -rf resources/views/templates/gmd >/dev/null 2>&1 &',
-            // 'rm -rf resources/views/templates/Carbon >/dev/null 2>&1 &',
             'rm -rf routes/custom >/dev/null 2>&1 &',
             'rm -rf routes/gmd.php >/dev/null 2>&1 &',
         ];
@@ -52,6 +49,9 @@ class UninstallCommand extends Command
         }
         exec('php artisan queue:restart');
         exec('php artisan up');
+        if ($installer == 0) {
+            exec('composer require wemx/installer');
+        }
 
         $this->info('Uninstalled the Billing Module and installed a fresh new Pterodactyl Panel');
     }
