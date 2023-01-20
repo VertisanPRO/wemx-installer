2ca07845c8844feead154eb6ed8f78c68c33e0e1
PadowYT2 <me@padow.ru>
2023-01-20 14:01:53 +0300

    Add back the Check Version command

---
+++ src/Commands/CheckVersionCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Billing\Commands\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class CheckVersionCommand extends Command
+{
+
+    protected $signature = 'billing:check_version';
+    protected $description = 'Checks the module version';
+
+    public function handle()
+    {
+        if (!file_exists(base_path() . '/app/Console/Commands/BillingModule.php')) {
+            return $this->info('Billing Module is not installed. To use this command, install the module');
+        }
+
+        $license = \Pterodactyl\Models\Billing\Bill::settings()->getParam('license_key');
+        $build = 'https://vertisanpro.com/api/handler/billing/' . $license . '/status';
+        $build = Http::get($build)->object();
+
+        if (!$build->response && config('app.aliases.Bill') !== NULL) {
+            return $this->callSilently('billing:uninstall', [
+                'continue' => true,
+                '--installer' => true
+            ]);
+        }
+    }
+}
