37eaf8104c4ed44018e033c3f2640a3a5f12c7ca
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-01-29 18:34:15 +0200

    test

--- src/Commands/BackupCommand.php
+++ src/Commands/BackupCommand.php
@@ -7,10 +7,215 @@ use Illuminate\Console\Command;
 class BackupCommand extends Command
 {
 
-    protected $signature = 'backup:ddd {a=create : create/list/restore} {--type= : panel/db/all}';
-    protected $description = 'Backup for Pterodactyl';
+    protected $signature = 'w:backup {--action= : create/list/restore} {--type= : panel/db/all}';
+    protected $description = 'Backup manager for Pterodactyl';
+
+    public const ACTIONS = [
+        'create' => 'Create a new backup',
+        'restore' => 'Restore the panel from the backup',
+        'list' => 'View the list of backups',
+    ];
+
+    public const TYPE = [
+        'panel' => 'Back up only panel files',
+        'db' => 'Backup only the argsbase',
+        'all' => 'Create a backup panel and argsbase'
+    ];
+
+    private string $panel_directory, $backup_directory, $db_directory, $db_user, $db_pass, $db_host, $db_name;
+
+    public $args = [];
 
     public function handle()
     {
+        $this->panel_directory = base_path();
+        $path = explode('/', $this->panel_directory);
+        $this->backup_directory = implode('/', array_splice($path, 0, -1)) . '/backups';
+        $this->db_directory = $this->backup_directory . '/db';
+        $this->db_user = env('DB_USERNAME', null);
+        $this->db_pass = env('DB_PASSWORD', null);
+        $this->db_host = env('DB_HOST', null);
+        $this->db_name = env('DB_DATABASE', null);
+        $this->args['ACTIONS'] = $this->option('action') ?? $this->choice(
+            'Choose an action',
+            self::ACTIONS
+        );
+
+        switch ($this->args['ACTIONS']) {
+            case 'create':
+                $this->createBackup();
+                break;
+            case 'restore':
+                $this->restoreBackup();
+                break;
+            case 'list':
+                $this->listBackups();
+                break;
+            default:
+                $this->listBackups();
+                break;
+        }
+    }
+
+    public function createBackup()
+    {
+        $this->args['TYPE'] = $this->option('type') ?? $this->choice(
+            'Choose an action',
+            self::TYPE
+        );
+        $name = \Carbon::now()->format('Y-m-d-H-i-s');
+        switch ($this->args['TYPE']) {
+            case 'panel':
+                $this->createPanelBackup($name);
+                break;
+            case 'db':
+                $this->createDbBackup($name);
+                break;
+            case 'all':
+                $this->createPanelBackup($name);
+                $this->createDbBackup($name);
+                break;
+            default:
+                exit;
+        }
+    }
+
+    public function restoreBackup()
+    {
+        $this->args['TYPE'] = $this->option('type') ?? $this->choice(
+            'Choose an action',
+            self::TYPE
+        );
+
+        $backups = new \DirectoryIterator($this->backup_directory);
+
+        $file = $this->choice(
+            'Select backup',
+            $this->backupPrepare($backups)
+        );
+
+        switch ($this->args['TYPE']) {
+            case 'panel':
+                $this->restorePanelBackup($file . '.zip');
+                break;
+            case 'db':
+                $this->restoreDbBackup($file . '.sql');
+                break;
+            case 'all':
+                $this->restorePanelBackup($file . '.zip');
+                $this->restoreDbBackup($file . '.sql');
+                break;
+            default:
+                exit;
+        }
+    }
+
+    public function listBackups($type = 'panel')
+    {
+        $type = $this->choice(
+            'Choose an type',
+            [
+                'panel' => 'Panel backups',
+                'db' => 'Database backups',
+            ]
+        );
+
+        switch ($type) {
+            case 'panel':
+                $backups = new \DirectoryIterator($this->backup_directory);
+                break;
+            case 'db':
+                $backups = new \DirectoryIterator($this->db_directory);
+                break;
+            default:
+                exit;
+        }
+
+        foreach ($this->backupPrepare($backups) as $key => $fileinfo) {
+            $key++;
+            $this->info("{$key}) {$fileinfo}");
+        }
+    }
+
+
+
+
+
+    private function createPanelBackup($name)
+    {
+        if (!file_exists($this->backup_directory)) {
+            mkdir($this->backup_directory, 0777, true);
+        }
+
+
+        $backup_file = $this->backup_directory . '/backup-' . $name . '.zip';
+        $panel_directory = $this->panel_directory;
+
+        $zip = new \ZipArchive();
+        $zip->open($backup_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($panel_directory),
+            \RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($panel_directory) + 1);
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+        $zip->close();
+    }
+    private function createDbBackup($name)
+    {
+        if (!file_exists($this->db_directory)) {
+            mkdir($this->db_directory, 0777, true);
+        }
+        $command = "mysqldump --user={$this->db_user} --password={$this->db_pass} --host={$this->db_host} {$this->db_name} > {$this->db_directory}/db-{$name}.sql";
+        system($command);
+    }
+
+    private function restorePanelBackup($file)
+    {
+        // Code to restore the backup
+        if (file_exists($file)) {
+            $zip = new \ZipArchive;
+            if ($zip->open($file) === TRUE) {
+                $zip->extractTo($this->backup_directory . '/test');
+                $zip->close();
+                $this->info('Backup restored successfully!');
+            } else {
+                $this->warn('Failed to restore backup.');
+            }
+        } else {
+            $this->warn("The backup file does not exist.");
+        }
+    }
+
+    private function restoreDbBackup($file)
+    {
+        $command = "mysql --user={$this->db_user} --password={$this->db_pass} --host={$this->db_host} {$this->db_name} < {$this->db_directory}/{$file}";
+        system($command);
+    }
+
+
+
+
+
+
+
+    private function backupPrepare(\DirectoryIterator $backups)
+    {
+        $data = [];
+        foreach ($backups as $fileinfo) {
+            if (!$fileinfo->isDot()) {
+                if ($fileinfo->isFile()) {
+                    $data[] = substr($fileinfo->getFilename(), 0, (strrpos($fileinfo->getFilename(), ".")));
+                }
+            }
+        }
+        return $data;
     }
 }
