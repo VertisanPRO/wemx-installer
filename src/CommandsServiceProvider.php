f22f5b39c4e7577cf286a656e1c5d9774cf2c515
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-01-31 12:25:39 +0200

    Update CommandsServiceProvider.php

--- src/CommandsServiceProvider.php
+++ src/CommandsServiceProvider.php
@@ -3,16 +3,16 @@
 namespace Wemx\Installer;
 
 use Illuminate\Support\ServiceProvider;
-use Wemx\Installer\Commands\InstallCommand;
-use Wemx\Installer\Commands\HelpCommand;
-use Wemx\Installer\Commands\UninstallCommand;
-use Wemx\Installer\Commands\FixCommand;
-use Wemx\Installer\Commands\YarnCommand;
-use Wemx\Installer\Commands\InstallphpMyAdmin;
+use Wemx\Installer\Commands\BackupCommand;
 use Wemx\Installer\Commands\CreateMySQLUser;
 use Wemx\Installer\Commands\DeleteMySQLUser;
+use Wemx\Installer\Commands\FixCommand;
+use Wemx\Installer\Commands\HelpCommand;
+use Wemx\Installer\Commands\InstallCommand;
+use Wemx\Installer\Commands\InstallphpMyAdmin;
 use Wemx\Installer\Commands\LicenseCommand;
-use Wemx\Installer\Commands\BackupCommand;
+use Wemx\Installer\Commands\UninstallCommand;
+use Wemx\Installer\Commands\YarnCommand;
 use Wemx\Installer\FileEditor;
 
 class CommandsServiceProvider extends ServiceProvider
@@ -29,7 +29,6 @@ class CommandsServiceProvider extends ServiceProvider
         }
 
         $this->publishes([__DIR__ . '/../config/wemx-backup.php' => config_path('wemx-backup.php')], 'wemx-backup');
-        $this->mergeConfig();
     }
 
     /**
@@ -38,22 +37,22 @@ class CommandsServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/aliases.php', 'app.aliases');
+        $this->mergeConfig();
+
     }
-
-
 
     private function mergeConfig()
     {
-        $this->wemx_backup = include(__DIR__ . '/../config/wemx-backup.php');
-        $wemx_backup = config('wemx-backup');
-        foreach ($this->wemx_backup as $key => $value) {
-            if (isset($wemx_backup[$key])) {
-                continue;
+        if (file_exists(config_path('wemx-backup.php'))) {
+            $this->wemx_backup = include __DIR__ . '/../config/wemx-backup.php';
+            $wemx_backup = config('wemx-backup');
+            foreach ($this->wemx_backup as $key => $value) {
+                if (isset($wemx_backup[$key])) {
+                    continue;
+                }
+                FileEditor::appendAfter(config_path('wemx-backup.php'), 'return [', "    '{$key}' => " . json_encode($value) . ",");
             }
-            $wemx_backup[$key] = $value;
         }
-        $file = new FileEditor(config_path('wemx-backup.php'));
-        $file->writeToFile($wemx_backup);
         return;
     }
 }
