34907062afd57faa4cf52680503c6bff2127a7ed
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-01-31 11:09:57 +0200

    test

--- src/CommandsServiceProvider.php
+++ src/CommandsServiceProvider.php
@@ -25,7 +25,7 @@ class CommandsServiceProvider extends ServiceProvider
             $this->commands([InstallCommand::class, HelpCommand::class, UninstallCommand::class, FixCommand::class, YarnCommand::class, InstallphpMyAdmin::class, CreateMySQLUser::class, DeleteMySQLUser::class, LicenseCommand::class, BackupCommand::class]);
         }
 
-        $this->publishes([__DIR__ . '/../config/wemx_backup.php' => config_path('wemx_backup.php')], 'config');
+        $this->publishes([__DIR__ . '/../config/wemx-backup.php' => config_path('wemx-backup.php')], 'config');
     }
 
     /**
@@ -33,7 +33,7 @@ class CommandsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/wemx_backup.php', 'wemx_backup');
+        // $this->mergeConfigFrom(__DIR__ . '/../config/wemx-backup.php', 'wemx-backup');
         $this->mergeConfigFrom(__DIR__ . '/../config/aliases.php', 'app.aliases');
     }
 }
