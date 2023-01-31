d3d3745f7bded6d88b2ecee48449f99b98d8c1d2
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-01-31 10:56:21 +0200

    Update wemx_backup.php

--- config/wemx_backup.php
+++ config/wemx_backup.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'backup_directory' => '',
-    'autobackup' => false,
+    'backup_directory' => '', // /home/backup/panel
+    'autobackup' => false, // true/false
     'autobackup_interval' => 24, // hours
 ];
