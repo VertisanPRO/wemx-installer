149f33436848fe9a63e7ab339100af765502ee85
Mubeen142 <mubeenullah323@gmail.com>
2023-07-07 14:41:47 +0200

    Update CommandsServiceProvider.php

--- src/CommandsServiceProvider.php
+++ src/CommandsServiceProvider.php
@@ -7,8 +7,6 @@ use Wemx\Installer\Commands\WemXInstaller;
 
 class CommandsServiceProvider extends ServiceProvider
 {
-
-    private $wemx_backup;
     /**
      * Bootstrap the application services.
      */
@@ -24,23 +22,6 @@ class CommandsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/aliases.php', 'app.aliases');
-        $this->mergeConfig();
 
-    }
-
-    private function mergeConfig()
-    {
-        if (file_exists(config_path('wemx-backup.php'))) {
-            $this->wemx_backup = include __DIR__ . '/../config/wemx-backup.php';
-            $wemx_backup = config('wemx-backup');
-            foreach ($this->wemx_backup as $key => $value) {
-                if (isset($wemx_backup[$key])) {
-                    continue;
-                }
-                FileEditor::appendAfter(config_path('wemx-backup.php'), 'return [', "    '{$key}' => " . json_encode($value) . ",");
-            }
-        }
-        return;
     }
 }
