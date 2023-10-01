8268c76fd4ac2b22189ee1628b575ffb89127bc0
Mubeen142 <mubeenullah323@gmail.com>
2023-10-01 15:24:29 +0200

    Update WemXInstaller.php

--- src/Commands/WemXInstaller.php
+++ src/Commands/WemXInstaller.php
@@ -10,7 +10,7 @@ class WemXInstaller extends Command
 {
     protected $description = 'Install wemx';
 
-    protected $signature = 'wemx:install {license_key?} {--type=stable} {--ver=latest} {--eula=no}';
+    protected $signature = 'wemx:install {license_key?} {--type=stable} {--ver=latest}';
 
     /**
      * WemXInstaller constructor.
@@ -35,14 +35,6 @@ class WemXInstaller extends Command
         ");
 
         $this->sshUser();
-
-        if ($this->getOption('eula', 'no') != 'yes'){
-            if (!$this->confirm('I have read https://wemx.net/pages/eula (EULA) and accept the terms', false)) {
-                return $this->error('You must agree to our EULA to continue');
-            }
-        }
-
-
 
         $license_key = $this->argument('license_key') ?? $this->ask("Please enter your license key", 'cancel');
 
