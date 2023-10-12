bdf7d015489c6f9606a6fb8363cae59653b654cb
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-12 16:54:25 +0300

    webserver commands

---
+++ src/Commands/Setup/SetupApacheCommand.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Wemx\Installer\Commands\Setup;
+
+use Illuminate\Console\Command;
+
+class SetupApacheCommand extends Command
+{
+    protected $signature = 'wemx:apache';
+    protected $description = 'Apache setup command';
+
+    protected string $domain;
+    protected string $rootPath = '/var/www/wemx/public';
+    protected string $apacheConfig;
+    protected bool $useSSL = false;
+
+    public function handle(): void
+    {
+        $this->info('Configuring Apache');
+        $this->askDomain();
+        $this->askRootPath();
+
+        if ($this->confirm('Would you like to configure SSL?', true)) {
+            $this->apacheConfig = $this->generateApacheSSLConfig();
+            $this->useSSL = true;
+        } else {
+            $this->apacheConfig = $this->generateApacheConfig();
+        }
+        if ($this->saveAndLinkApacheConfig()) {
+            if ($this->useSSL) {
+                $this->installSSL();
+                shell_exec("sudo a2enmod ssl");
+            }
+            shell_exec("sudo systemctl restart apache2");
+            $this->info('Apache configuration is complete');
+        }
+    }
+
+    private function askRootPath(): void
+    {
+        $defaultPath = $this->rootPath;
+        $this->rootPath = $this->askWithCompletion('Please enter the root path to your Laravel project or press Enter to accept the default path:', [], $defaultPath);
+        while (!is_dir($this->rootPath)) {
+            $this->error('Invalid path. Please try again.');
+            $this->rootPath = $this->askWithCompletion('Please enter the root path to your Laravel project or press Enter to accept the default path:', [], $defaultPath);
+        }
+    }
+
+    private function askDomain(): void
+    {
+        $this->newLine();
+        $this->domain = $this->ask('Please enter your domain without http:// or https:// (e.g., example.com)');
+        while (!preg_match('/^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/', $this->domain)) {
+            $this->error('Invalid domain. Please try again.');
+            $this->domain = $this->ask('Please enter your domain without http:// or https:// (e.g., example.com)');
+        }
+    }
+
+    private function installSSL(): void
+    {
+        $this->info('Checking for Certbot and its Nginx plugin...');
+        $needToInstall = shell_exec("dpkg -l | grep -E 'certbot|python3-certbot-nginx'") === null;
+        if ($needToInstall) {
+            $this->info('Installing Certbot and its Nginx plugin...');
+            shell_exec("sudo apt-get install certbot python3-certbot-nginx -y");
+        }
+        $this->info('Obtaining an SSL certificate...');
+        shell_exec("sudo certbot --nginx -d {$this->domain}");
+        $this->info('SSL certificate installed successfully.');
+    }
+
+    private function saveAndLinkApacheConfig(): bool
+    {
+        $configPath = "/etc/apache2/sites-available/{$this->domain}.conf";
+        if (file_exists($configPath)) {
+            $this->error("The configuration file {$configPath} already exists. Aborting.");
+            return false;
+        }
+        file_put_contents($configPath, $this->apacheConfig);
+        shell_exec("sudo ln -s /etc/apache2/sites-available/{$this->domain}.conf /etc/apache2/sites-enabled/{$this->domain}.conf");
+        shell_exec("sudo a2enmod rewrite");
+        $this->info('Apache configuration saved and linked successfully. Apache has been restarted.');
+        return true;
+    }
+
+    private function generateApacheSSLConfig(): string
+    {
+        return <<<EOL
+<VirtualHost *:80>
+  ServerName {$this->domain}
+
+  RewriteEngine On
+  RewriteCond %{HTTPS} !=on
+  RewriteRule ^/?(.*) https://%{SERVER_NAME}/$1 [R,L]
+</VirtualHost>
+
+<VirtualHost *:443>
+  ServerName {$this->domain}
+  DocumentRoot "{$this->rootPath}"
+
+  AllowEncodedSlashes On
+
+  php_value upload_max_filesize 100M
+  php_value post_max_size 100M
+
+  <Directory "{$this->rootPath}">
+    Require all granted
+    AllowOverride all
+  </Directory>
+
+  SSLEngine on
+  SSLCertificateFile /etc/letsencrypt/live/{$this->domain}/fullchain.pem
+  SSLCertificateKeyFile /etc/letsencrypt/live/{$this->domain}/privkey.pem
+</VirtualHost>
+EOL;
+    }
+
+    private function generateApacheConfig(): string
+    {
+        return <<<EOL
+<VirtualHost *:80>
+  ServerName {$this->domain}
+  DocumentRoot "{$this->rootPath}"
+
+  AllowEncodedSlashes On
+
+  php_value upload_max_filesize 100M
+  php_value post_max_size 100M
+
+  <Directory "{$this->rootPath}">
+    AllowOverride all
+    Require all granted
+  </Directory>
+</VirtualHost>
+EOL;
+    }
+
+}
---
+++ src/Commands/Setup/SetupNginxCommand.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Wemx\Installer\Commands\Setup;
+
+use Illuminate\Console\Command;
+
+class SetupNginxCommand extends Command
+{
+    protected $signature = 'wemx:nginx';
+    protected $description = 'Nginx setup command';
+
+    protected string $domain;
+    protected string $phpSocket = '/run/php/php8.1-fpm.sock';
+    protected string $rootPath = '/var/www/wemx/public';
+    protected string $nginxConfig;
+    protected bool $useSSL = false;
+
+    public function handle(): void
+    {
+        $this->info('Configuring Nginx');
+        $this->askDomain();
+        $this->askRootPath();
+        $this->checkPhpSocket();
+
+        if ($this->confirm('Would you like to configure SSL?', true)) {
+            $this->nginxConfig = $this->generateNginxSSLConfig();
+            $this->useSSL = true;
+        } else {
+            $this->nginxConfig = $this->generateNginxConfig();
+        }
+        if ($this->saveAndLinkNginxConfig()){
+            if ($this->useSSL){
+                $this->installSSL();
+            }
+            shell_exec("sudo systemctl restart nginx");
+            $this->info('Nginx configuration is complete');
+        }
+    }
+
+    private function askRootPath(): void
+    {
+        $defaultPath = $this->rootPath;
+        $this->rootPath = $this->askWithCompletion('Please enter the root path to your Laravel project or press Enter to accept the default path:', [], $defaultPath);
+        while (!is_dir($this->rootPath)) {
+            $this->error('Invalid path. Please try again.');
+            $this->rootPath = $this->askWithCompletion('Please enter the root path to your Laravel project or press Enter to accept the default path:', [], $defaultPath);
+        }
+    }
+    private function askDomain(): void
+    {
+        $this->newLine();
+        $this->domain = $this->ask('Please enter your domain without http:// or https:// (e.g., example.com)');
+        while (!preg_match('/^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/', $this->domain)) {
+            $this->error('Invalid domain. Please try again.');
+            $this->domain = $this->ask('Please enter your domain without http:// or https:// (e.g., example.com)');
+        }
+    }
+    private function checkPhpSocket(): void
+    {
+        if (!file_exists($this->phpSocket)) {
+            $this->error('PHP socket not found.');
+            if ($this->confirm('Would you like to install PHP-FPM to create the socket?')) {
+                $this->installPhpFpm();
+            }
+        }
+    }
+    private function installPhpFpm(): void
+    {
+        $this->info('Installing PHP-FPM...');
+        shell_exec('sudo apt-get install php8.1-fpm -y');
+        $this->info('PHP-FPM installed successfully.');
+    }
+    private function installSSL(): void
+    {
+        $this->info('Checking for Certbot and its Nginx plugin...');
+        $needToInstall = shell_exec("dpkg -l | grep -E 'certbot|python3-certbot-nginx'") === null;
+        if ($needToInstall) {
+            $this->info('Installing Certbot and its Nginx plugin...');
+            shell_exec("sudo apt-get install certbot python3-certbot-nginx -y");
+        }
+        $this->info('Obtaining an SSL certificate...');
+        shell_exec("sudo certbot --nginx -d {$this->domain}");
+        $this->info('SSL certificate installed successfully.');
+    }
+    private function saveAndLinkNginxConfig(): bool
+    {
+        $configPath = "/etc/nginx/sites-available/{$this->domain}.conf";
+        if (file_exists($configPath)) {
+            $this->error("The configuration file {$configPath} already exists. Aborting.");
+            return false;
+        }
+        file_put_contents($configPath, $this->nginxConfig);
+        shell_exec("sudo ln -s {$configPath} /etc/nginx/sites-enabled/{$this->domain}.conf");
+        $this->info('Nginx configuration saved and linked successfully. Nginx has been restarted.');
+        return true;
+    }
+    private function generateNginxSSLConfig(): string
+    {
+        return <<<EOL
+server {
+    listen 80;
+    server_name {$this->domain};
+    server_tokens off;
+    return 301 https://\$server_name\$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    server_name {$this->domain};
+
+    root {$this->rootPath};
+    index index.php;
+
+    access_log /var/log/nginx/wemx.app-access.log;
+    error_log  /var/log/nginx/wemx.app-error.log error;
+
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    ssl_certificate /etc/letsencrypt/live/{$this->domain}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/{$this->domain}/privkey.pem;
+    ssl_session_cache shared:SSL:10m;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
+    ssl_prefer_server_ciphers on;
+
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header X-Frame-Options DENY;
+    add_header Referrer-Policy same-origin;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:{$this->phpSocket};
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+        include /etc/nginx/fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOL;
+    }
+    private function generateNginxConfig(): string
+    {
+        return <<<EOL
+server {
+    listen 80;
+    server_name {$this->domain};
+
+    root {$this->rootPath};
+    index index.html index.htm index.php;
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log off;
+    error_log  /var/log/nginx/wemx.app-error.log error;
+
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:{$this->phpSocket};
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+EOL;
+    }
+
+}
