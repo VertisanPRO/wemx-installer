1978cc221eadb8581b829ecce7727455fdb4b46b
Mubeen142 <58806240+Mubeen142@users.noreply.github.com>
2023-10-13 00:03:57 +0200

    Update CommandsServiceProvider.php

--- src/CommandsServiceProvider.php
+++ src/CommandsServiceProvider.php
@@ -8,12 +8,16 @@ use Wemx\Installer\Commands\WemXInstaller;
 use Wemx\Installer\Commands\WemXUpdate;
 use Wemx\Installer\Http\Middleware\CheckAppInstalled;
 
+use Illuminate\Console\Events\Scheduling;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Contracts\Events\Dispatcher;
+
 class CommandsServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(Dispatcher $events)
     {
         $this->commands([
             WemXInstaller::class,
@@ -24,6 +28,10 @@ class CommandsServiceProvider extends ServiceProvider
         $this->app['router']->aliasMiddleware('app_installed', CheckAppInstalled::class);
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
         $this->loadViewsFrom(__DIR__.'/Views', 'installer');
+
+        $events->listen(Scheduling::class, function (Scheduling $event) {
+            $this->schedule($event->schedule);
+        });
     }
 
     /**
@@ -32,5 +40,10 @@ class CommandsServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+
+    protected function schedule(Schedule $schedule)
+    {
+        // add commands
     }
 }
