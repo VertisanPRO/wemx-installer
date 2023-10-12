0340b3a2474c79112ace1473bc7a1e4d1f616bb9
Mubeen142 <mubeenullah323@gmail.com>
2023-10-12 18:52:12 +0200

    Update install.blade.php

--- src/Views/install.blade.php
+++ src/Views/install.blade.php
@@ -32,7 +32,7 @@
         <div>
             <label for="license" class="block text-sm font-medium leading-6 text-gray-900">License Key</label>
             <div class="mt-2">
-              <input type="text" name="license" id="license" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="you@example.com" aria-describedby="email-description">
+              <input type="text" name="license" id="license" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="WEMX-XXXXXXXXXXXXXXXXXXX" aria-describedby="email-description">
             </div>
             <p class="mt-2 text-sm text-gray-500" id="license-description">Please enter your license key.</p>
         </div>
