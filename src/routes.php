0b20ce5734c584156f5fdb344c41adb24822e977
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-13 23:16:43 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Wemx\Installer\Controllers\InstallController;
 
 Route::controller(InstallController::class)->prefix('install')->group(function () {
+    Route::get('/', 'requirements');
     Route::get('/requirements', 'requirements');
     Route::get('/download', 'download');
 });
