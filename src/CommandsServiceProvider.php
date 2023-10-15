c6f38284baf1b3ed5201225b13f51ca8b37b145e
Mubeen142 <mubeenullah323@gmail.com>
2023-10-15 19:00:36 +0200

    revert changes to default

--- src/CommandsServiceProvider.php
+++ src/CommandsServiceProvider.php
@@ -4,49 +4,26 @@ namespace Wemx\Installer;
 
 use Illuminate\Support\ServiceProvider;
 use Wemx\Installer\Commands\PingCommand;
-use Wemx\Installer\Commands\Setup\DatabaseSettingsCommand;
-use Wemx\Installer\Commands\Setup\SetupApacheCommand;
-use Wemx\Installer\Commands\Setup\SetupCommand;
-use Wemx\Installer\Commands\Setup\SetupDatabaseCommand;
-use Wemx\Installer\Commands\Setup\SetupNginxCommand;
-use Wemx\Installer\Commands\Setup\SetWebChownCommand;
 use Wemx\Installer\Commands\WemXInstaller;
 use Wemx\Installer\Commands\WemXUpdate;
-use Wemx\Installer\Commands\QueueCommands;
-use Wemx\Installer\Middleware\CheckAppInstalled;
-
-use Illuminate\Console\Events\Scheduling;
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Contracts\Events\Dispatcher;
 
 class CommandsServiceProvider extends ServiceProvider
 {
-    public function boot(Dispatcher $events)
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
     {
         $this->commands([
             WemXInstaller::class,
             WemXUpdate::class,
-            QueueCommands::class,
-            WemXUpdate::class,
-            DatabaseSettingsCommand::class,
-            SetupCommand::class,
-            SetupDatabaseCommand::class,
-            SetupNginxCommand::class,
-            SetupApacheCommand::class,
-            SetWebChownCommand::class,
+            PingCommand::class,
         ]);
-
-        $this->app['router']->aliasMiddleware('app_installed', CheckAppInstalled::class);
-        $this->loadRoutesFrom(__DIR__ . '/routes.php');
-        $this->loadViewsFrom(__DIR__.'/Views', 'installer');
-
-        $this->app->booted(function () {
-            $schedule = $this->app->make(Schedule::class);
-            $schedule->command('queue:commands')->everyFiveSeconds()->withoutOverlapping();
-        });
-
     }
 
+    /**
+     * Register the application services.
+     */
     public function register()
     {
 
