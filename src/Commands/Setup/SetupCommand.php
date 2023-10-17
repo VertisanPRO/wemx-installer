f9ee11ca3d5e0107d61ad75a8e87095fcd444198
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2023-10-17 18:08:21 +0300

    Update SetupCommand.php

--- src/Commands/Setup/SetupCommand.php
+++ src/Commands/Setup/SetupCommand.php
@@ -68,9 +68,9 @@ class SetupCommand extends Command
             Config::set('app.key', $this->app_key);
         }
 
-        $this->setupWebServer();
         $this->setupDatabase();
-
+        $this->setupWebServer();
+        
         $this->warn('WemX Installation');
         $this->call('wemx:install', ['license_key' => $this->license_key, '--type' => $this->type], $this->output);
         passthru('composer install --optimize-autoloader --ansi -n');
