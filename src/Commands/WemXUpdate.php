4ceafa91f5cdf9de329048570bef5452dcc693df
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-03-20 19:40:10 +0200

    Update WemXUpdate.php

--- src/Commands/WemXUpdate.php
+++ src/Commands/WemXUpdate.php
@@ -110,7 +110,7 @@ class WemXUpdate extends Command
 
         $this->info('Updating webserver permissions');
         system('php artisan wemx:chown');
-        system('php artisan queue:start --force');
+        system('php artisan queue:start --force > /dev/null 2>&1');
 
         $this->info('Updating license key');
         system('php artisan license:update '. $license_key);
