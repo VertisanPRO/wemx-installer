1436183dc085d80ab8511016fd87c1a7675a20e0
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-03-26 16:18:43 +0200

    Update nginx.sh

--- src/nginx.sh
+++ src/nginx.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# source  <(curl -s https://raw.githubusercontent.com/VertisanPRO/wemx-installer/web/src/nginx.sh)
+# source <(curl -s https://raw.githubusercontent.com/VertisanPRO/wemx-installer/web/src/nginx.sh)
 apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
 curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 apt update
 echo | apt-add-repository universe
-apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server-10.5 nginx tar unzip git
+apt -y install php8.1 php8.1-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server-10.3 nginx tar unzip git
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 export COMPOSER_ALLOW_SUPERUSER=1
 cd /var/www
